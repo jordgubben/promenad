4769c91614ef0d2902e15d319028d4d82ae59e78
Jakob Eklund <jakob.eklund@superattack.se>
2021-03-08 11:03:27 +0100

    Render a red sphere (on a grid) at origo

--- src/view.c
+++ src/view.c
@@ -1,3 +1,5 @@
+#include <raylib.h>
+
 #define IN_VIEW
 #include "overview.h"
 
@@ -5,5 +7,23 @@
 Render all the things.
 **/
 void render_app(const app_t *app) {
+	// Define the camera to look into our 3d world
+	Camera3D camera = { 0 };
+	camera.position = (Vector3){ 10.0f, 10.0f, 10.0f };
+	camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
+	camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+	camera.fovy = 45.0f;
+	camera.type = CAMERA_PERSPECTIVE;
+	SetCameraMode(camera, CAMERA_FREE);
+	UpdateCamera(&camera);
 
+	// Render something at origo
+	BeginMode3D(camera);
+	{
+		Vector3 origo = { 0,0,0};
+		DrawSphere(origo, 1, RED);
+
+		DrawGrid(10, 1.f);
+	}
+	EndMode3D();
 }
