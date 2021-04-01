d16bd766c2ee83b3036c5e689ae57e3d01b7e9e9
Jakob Eklund <jakob.eklund@superattack.se>
2021-04-01 09:43:14 +0200

    Move camera a bit closer (see the arms better)

--- src/app_root.c
+++ src/app_root.c
@@ -24,7 +24,7 @@ int main(int argc, char** argv) {
 
 	// Define the camera to look into our 3d world
 	Camera3D camera = { 0 };
-	camera.position = (Vector3){ 15.0f, 5.0f, 35.0f };
+	camera.position = (Vector3){ 10.0f, 5.0f, 25.0f };
 	camera.target = (Vector3){ 5.0f, 0.0f, 0.0f };
 	camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
 	camera.fovy = 45.0f;
